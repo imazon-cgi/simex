79dd62a73f52752c07469732814022595693e993
bluebellearth <bluebellearth@bluebellindex.com>
2025-10-17 20:51:32 -0300

    ajustes

--- server.js
+++ server.js
@@ -41,8 +41,7 @@ const cspDirectives = {
     "https://cdn.jsdelivr.net",
     "https://unpkg.com",
     "https://cdnjs.cloudflare.com",
-    "https://cdn.datatables.net",
-    "https://d3js.org"
+    "https://cdn.datatables.net"
   ],
 
   "style-src": [
@@ -63,27 +62,26 @@ const cspDirectives = {
   ],
 
   "img-src": [
-  "'self'",
-  "data:",
-  "blob:",
-  "https://*.tile.openstreetmap.org",
-  "https://*.basemaps.cartocdn.com",   // necessário para tiles CARTO
-  "https://unpkg.com",
-  "https://cdn.jsdelivr.net",
-  "https://cdnjs.cloudflare.com",
-  "https://cdn.datatables.net",
-  "https://imazongeo3-web.s3.sa-east-1.amazonaws.com"
-],
-"connect-src": [
-  "'self'",
-  "https://*.tile.openstreetmap.org",
-  "https://*.basemaps.cartocdn.com",   // alguns navegadores consultam aqui também para tiles
-  "https://cdn.datatables.net",
-  "https://cdnjs.cloudflare.com",
-  "https://cdn.jsdelivr.net",
-  "https://unpkg.com"
-],
+    "'self'",
+    "data:",
+    "blob:",
+    "https://*.tile.openstreetmap.org",
+    "https://*.basemaps.cartocdn.com",
+    "https://unpkg.com",
+    "https://cdn.jsdelivr.net",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.datatables.net",
+    "https://imazongeo3-web.s3.sa-east-1.amazonaws.com"
+  ],
 
+  "connect-src": [
+    "'self'",
+    "https://*.tile.openstreetmap.org",
+    "https://cdn.datatables.net",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+    "https://unpkg.com"
+  ],
 
   "worker-src": ["'self'", "blob:"],
   "object-src": ["'none'"],
